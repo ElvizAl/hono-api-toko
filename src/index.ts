94b211417d31c046f9b24d355f24adfdc48fa005
elvizsky-23 <elvizskuy@gmail.com>
2026-03-15 16:57:48 +0800

    percobaan

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,33 @@ import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 import { authRouter } from "./modules/auth/auth.route";
+import "dotenv";
 
 const app = new Hono().basePath("api").route("/auth", authRouter);
 
-app.use("*", logger());
+app.use(logger());
 
 app.use(
-	"*",
-	cors({
-		origin: ["http://localhost:3000"],
-		allowMethods: ["GET", "POST", "PUT", "DELETE"],
-		allowHeaders: ["Content-Type", "Authorization"],
-		credentials: true,
-	}),
+  "*",
+  cors({
+    origin: ["https://hono-api-toko.vercel.app/"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
 );
 
 app.notFound((c) => {
-	return c.json({ message: "Tidak Ditemukan" }, 404);
+  return c.json({ message: "Tidak Ditemukan" }, 404);
 });
 
 app.onError((err, c) => {
-	if (err instanceof HTTPException) {
-		return c.json({ message: err.message }, err.status);
-	}
+  if (err instanceof HTTPException) {
+    return c.json({ message: err.message }, err.status);
+  }
 
-	console.error("Internal Server Error:", err);
-	return c.json({ message: "Internal Server Error" }, 500);
+  console.error("Internal Server Error:", err);
+  return c.json({ message: "Internal Server Error" }, 500);
 });
 
 export default app;
