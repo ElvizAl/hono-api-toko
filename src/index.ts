1bc3ab244266e584abd61622866223e5e6ec3ddc
elvizsky-23 <elvizskuy@gmail.com>
2026-03-15 21:56:26 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(
   "*",
   cors({
     origin: ["http://localhost:3000"],
-    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
